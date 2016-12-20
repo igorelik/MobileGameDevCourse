da27820837ec50445f9c06f03cf5a16582623e04
Igor Gorelik <gorelik.igor@gmail.com>
2016-12-20 12:14:41 +0200

    no message

--- Unity5 - Roll a Ball game Tutorial/RollABall/Assets/Scripts/PlayerController.cs
+++ Unity5 - Roll a Ball game Tutorial/RollABall/Assets/Scripts/PlayerController.cs
@@ -39,26 +39,6 @@ public class PlayerController : MonoBehaviour {
 		}
 
 	}
-//	void OnCollisionEnter(Collision collision)
-//	{
-//		//Debug.Log (string.Format ("Player collided with {0}", collision.gameObject.tag));
-//		if (collision.gameObject.CompareTag ( "Wall"))
-//		{
-//			Debug.Log (string.Format ("Wall: input velocity : {0} {1} {2}", rb.velocity.x, rb.velocity.y, rb.velocity.z));
-//			rb.
-////			float moveHorizontal = Input.GetAxis ("Horizontal");
-////			float moveVertical = Input.GetAxis ("Vertical");
-////
-////			Vector3 movement = new Vector3 (-moveHorizontal, 0.0f, -moveVertical);
-////
-////			rb.AddForce (movement * speed);
-//			//rb.velocity = rb.velocity * -10;
-//
-//			rb.velocity = new Vector3 (rb.velocity.x * 1, -rb.velocity.y, -rb.velocity.z * 10);
-//
-//			Debug.Log (string.Format ("Wall: output velocity : {0} {1} {2}", rb.velocity.x, rb.velocity.y, rb.velocity.z));
-//		}
-//	}
 
 	void SetCountText ()
 	{
