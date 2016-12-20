739560a552849bc1014ca43e535afcd94302ae82
Igor Gorelik <igor.gorelik@icarehealth.com>
2016-12-20 12:46:21 +0200

    Game- start

--- Unity5 - Roll a Ball game Tutorial/RollABall/Assets/Scripts/PlayerController.cs
+++ Unity5 - Roll a Ball game Tutorial/RollABall/Assets/Scripts/PlayerController.cs
@@ -3,50 +3,50 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
-	public float speed;
-	public Text countText;
-	public Text winText;
-
-	private Rigidbody rb;
-	private int count;
-
-	void Start ()
-	{
-		rb = GetComponent<Rigidbody>();
-		count = 0;
-		SetCountText ();
-		winText.gameObject.SetActive (false);
-	}
-
-	void FixedUpdate ()
-	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-
-		rb.AddForce (movement * speed);
-	}
-
-	void OnTriggerEnter(Collider other) 
-	{
-		Debug.Log (string.Format ("Player trigger {0}", other.gameObject.tag));
-		if (other.gameObject.CompareTag ( "Pick Up"))
-		{
-			other.gameObject.SetActive (false);
-			count = count + 1;
-			SetCountText ();
-		}
-
-	}
-
-	void SetCountText ()
-	{
-		countText.text = "Count: " + count.ToString ();
-		if (count >= 10)
-		{
-			//winText.text = "You Win!";
-			winText.gameObject.SetActive(true);
-		}
-	}
+//	public float speed;
+//	public Text countText;
+//	public Text winText;
+//
+//	private Rigidbody rb;
+//	private int count;
+//
+//	void Start ()
+//	{
+//		rb = GetComponent<Rigidbody>();
+//		count = 0;
+//		SetCountText ();
+//		winText.gameObject.SetActive (false);
+//	}
+//
+//	void FixedUpdate ()
+//	{
+//		float moveHorizontal = Input.GetAxis ("Horizontal");
+//		float moveVertical = Input.GetAxis ("Vertical");
+//
+//		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+//
+//		rb.AddForce (movement * speed);
+//	}
+//
+//	void OnTriggerEnter(Collider other) 
+//	{
+//		Debug.Log (string.Format ("Player trigger {0}", other.gameObject.tag));
+//		if (other.gameObject.CompareTag ( "Pick Up"))
+//		{
+//			other.gameObject.SetActive (false);
+//			count = count + 1;
+//			SetCountText ();
+//		}
+//
+//	}
+//
+//	void SetCountText ()
+//	{
+//		countText.text = "Count: " + count.ToString ();
+//		if (count >= 10)
+//		{
+//			//winText.text = "You Win!";
+//			winText.gameObject.SetActive(true);
+//		}
+//	}
 }
